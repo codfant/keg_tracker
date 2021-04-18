28fefeafed0f83422cc28df7bf25b78e506fe48d
codfant <c.fant@fi.com>
2021-04-18 10:19:22 -0700

    Adding in a tap list (credit to W3 schools for their template).

--- keg_tracker/api/keg_tracker_api.py
+++ keg_tracker/api/keg_tracker_api.py
@@ -1,35 +1,30 @@
 from flask import Flask, Request, render_template, jsonify, redirect
-import flask_login
 from keg_tracker.config import ConfigDict
 import os
-from pathlib import Path
+import pyqrcode
+
+
 
 conf_dict = ConfigDict().conf_dict
-
 config_path = '../config.py'
 app = Flask(__name__, instance_relative_config=False)
 app.config.from_pyfile(config_path)
+
 
 @app.route('/', methods=['GET'])
 def main_reroute():
     return redirect('index.html')
 
+
 @app.route('/index.html', methods=['GET'])
 def index_html():
-    p = Path('/sys/bus/w1/devices/')
-    sensor_dir = ''
-    for entry in p.iterdir():
-        if '28-' in str(entry):
-            sensor_dir = str(entry)
-    if sensor_dir is not '':
-        with open(f"{sensor_dir}/w1_slave", mode='r') as temp_file:
-            temp_str = str(temp_file.read())
-        temp = int(float(temp_str.split('t=')[1].replace('\n', '')) / 1000) * 9/5 + 32
-    return render_template('index.html', flask=f'Temp is {temp} degrees F')
+        return render_template('index.html', flask=f'Temp sensor not found.')
+
 
 @app.route('/setup_keg.html', methods=['GET'])
 def setup_keg():
     return render_template('setup_keg.html')
+
 
 def main():
     HOST = os.getenv('SERVER_HOST', '0.0.0.0')
@@ -51,5 +46,6 @@ def main():
 
 
 if '__main__' == __name__:
-    print(type(conf_dict))
+    url = pyqrcode.create(conf_dict['site_url'])
+    url.svg('./static/img/qr_code.svg', scale=8)
     main()
